e69dacf901ed58ab7309257ae7c8ab8ffb7146ce
Oakesy76 <2404574@leedstrinity.ac.uk>
2025-10-10 11:37:08 +0100

    Friday and Monday Week 1

--- Week2M/Week2M/obj/Debug/net8.0/Week2M.AssemblyInfo.cs
+++ Week2M/Week2M/obj/Debug/net8.0/Week2M.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week2M")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00447944088c4201b87abcfe80351474562381e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e8a4fbdcffac349c57dfc8be691e3f4838e026")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week2M")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week2M")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
